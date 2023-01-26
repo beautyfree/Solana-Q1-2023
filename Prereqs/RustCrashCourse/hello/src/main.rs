317d6d6cea8e9aa985ad0a8e470b70048711fa1e
Alexey Elizarov <felex-ae@ya.ru>
2023-01-26 21:44:36 +0300

    feat(prereq/rustcrash): enums

--- Prereqs/RustCrashCourse/hello/src/main.rs
+++ Prereqs/RustCrashCourse/hello/src/main.rs
@@ -9,7 +9,8 @@
 // mod loops;
 // mod functions;
 // mod pointer_ref;
-mod structs;
+// mod structs;
+mod enums;
 
 fn main() {
     // print::run();
@@ -22,5 +23,6 @@ fn main() {
     // conditionals::run();
     // loops::run();
     // functions::run();
-    structs::run();
+    // structs::run();
+    enums::run();
 }
